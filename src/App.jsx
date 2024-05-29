80ba2fcf7c6655f935126dc8474d16d8556f801b
carolreedcruz <70779039+carolreedcruz@users.noreply.github.com>
2024-05-30 00:57:48 +0200

    refactor homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Articles from "./components/Articles";
-import AddArticle from "./components/AddArticle";
 import Navbar from "./components/Navbar";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,6 +6,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import EditArticle from "./components/EditArticle";
 import Article from "./components/Article";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
@@ -20,25 +17,8 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/signin" element={<Login />} />
           <Route path="/article/:id" element={<Article />} />
-          <Route
-            path="/"
-            element={
-              <div className="flex justify-center p-4 bg-gray-100">
-                <div className="flex flex-col w-full max-w-5xl p-4">
-                  <div className="flex flex-col md:flex-row">
-                    <div className="flex flex-col w-full md:w-2/3 p-4">
-                      <Articles />
-                    </div>
-                    <div className="flex items-center w-full md:w-1/3 p-4">
-                      <AddArticle />
-                    </div>
-                  </div>
-                </div>
-                <ToastContainer />
-              </div>
-            }
-          />
-          <Route path="/edit/:id" element={<EditArticle />} /> 
+          <Route path="/" element={<Homepage />} />
+          <Route path="/edit/:id" element={<EditArticle />} />
         </Routes>
       </Router>
     </div>
